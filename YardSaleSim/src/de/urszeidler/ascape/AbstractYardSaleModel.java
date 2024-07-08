657d1f96432c41239a4567ed9ef281f2b129e6b3
urs <urs@urszeidler.de>
2024-07-08 06:39:59 +0200

    add gini

--- YardSaleSim/src/de/urszeidler/ascape/AbstractYardSaleModel.java
+++ YardSaleSim/src/de/urszeidler/ascape/AbstractYardSaleModel.java
@@ -7,6 +7,12 @@
 package de.urszeidler.ascape;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.ascape.model.HostCell;
 import org.ascape.model.Scape;
@@ -15,6 +21,7 @@ import org.ascape.model.space.Array2DVonNeumann;
 import org.ascape.model.space.Coordinate2DDiscrete;
 import org.ascape.util.data.StatCollector;
 import org.ascape.util.data.StatCollectorCSAMM;
+import org.ascape.util.data.StatCollectorCalculated;
 import org.ascape.util.data.StatCollectorCond;
 import org.ascape.view.vis.ChartView;
 import org.ascape.view.vis.Overhead2DView;
@@ -62,6 +69,18 @@ public abstract class AbstractYardSaleModel extends Scape {
 		}
 	}
 
+	private final class StatCollectorGini extends StatCollectorCalculated {
+		private static final long serialVersionUID = 819462462218968871L;
+
+		public StatCollectorGini() {
+			super("gini");
+		}
+
+		public double calculateValue() {
+			return calculateGini();
+		}
+	}
+
 	protected int nAgents = 500;
 	protected double startWealth = 100;
 
@@ -78,7 +97,7 @@ public abstract class AbstractYardSaleModel extends Scape {
 	private Overhead2DView overheadView;
 
 	protected abstract AbstractYardSaleAgent createAgent();
-	
+
 	@Override
 	public void createScape() {
 		super.createScape();
@@ -108,11 +127,16 @@ public abstract class AbstractYardSaleModel extends Scape {
 		chart.addSeries("Minimum wealth", Color.red);
 		chart.addSeries("Maximum wealth", Color.blue);
 		chart.addSeries("Average wealth", Color.green);
-	
+
 		chart = new ChartView("poor and rich tracker");
 		agentScape.addView(chart);
 		chart.addSeries("Count poor", Color.blue);
 		chart.addSeries("Count super rich", Color.gray);
+
+		chart = new ChartView("gini");
+		agentScape.addView(chart);
+		chart.addSeries("Sum gini", Color.yellow);
+
 		overheadView = new Overhead2DView();
 		overheadView.setCellSize(15);
 		lattice.addView(overheadView);
@@ -126,7 +150,10 @@ public abstract class AbstractYardSaleModel extends Scape {
 		StatCollector countPoor = new StatCollectorPoor();
 		StatCollector countSuperrich = new StatCollectorSuperrich();
 		StatCollectorCSAMM wealthCollector = new StatCollectorWealth();
-	
+
+		StatCollectorGini statCollectorGini = new StatCollectorGini();
+		players.addStatCollector(statCollectorGini);
+
 		players.addStatCollector(countPoor);
 		players.addStatCollector(countSuperrich);
 		players.addStatCollector(wealthCollector);
@@ -178,6 +205,52 @@ public abstract class AbstractYardSaleModel extends Scape {
 
 	public void setPoorfactor(double poorfactor) {
 		this.poorfactor = poorfactor;
+	}
+
+	private double calculateGini() {
+		Double completeWealth = nAgents * startWealth;
+		List<Double> agentWealth = new ArrayList<>(nAgents);
+		agentScape.iterator().forEachRemaining(a -> {
+			AbstractYardSaleAgent ysa = (AbstractYardSaleAgent) a;
+			agentWealth.add(ysa.wealth);
+		});
+		if (agentWealth.size() < 100) {
+			System.err.println("less than 100 agents skip gini calculation");
+			return 0.0D;
+		}
+		List<Double> agentWealthSortedByWealth = agentWealth.stream().sorted().collect(Collectors.toList());
+		Map<Integer, List<Double>> wealthByPercent = wealthByPercent(agentWealthSortedByWealth);
+
+		List<Double> precentWealth = IntStream.range(0, wealthByPercent.size()).mapToObj(i -> {
+			Double reduce = wealthByPercent.get(i).stream().reduce(0.0D, (a, b) -> a + (b / completeWealth));
+			return reduce;
+		}).collect(Collectors.toList());
+		return lorenzToGini(precentWealth);
+	}
+
+	private double lorenzToGini(List<Double> precentWealth) {
+		double gini = 0.0D;
+		double currentWealth = 0.0D;
+		for (int i = 0; i < 100; i++) {
+			Double wealth = precentWealth.get(i);
+			Double normWealth = ((double) i + 1) / 100;
+			currentWealth = currentWealth + wealth;
+			Double diff = normWealth - currentWealth;
+//			gini = gini + 2 * diff / 100;//do this once not all time
+			gini = gini + diff;
+		}
+		return gini / 50.0D; // shortcut for multiply by 2 and divide by 100
+	}
+
+	private Map<Integer, List<Double>> wealthByPercent(List<Double> list) {
+		Map<Integer, List<Double>> wealthByPercent = new HashMap<>();
+		IntStream.range(0, list.size()).forEach(i -> {
+			Double currentPercent = ((double) i / (double) nAgents) * 100;
+			List<Double> orDefault = wealthByPercent.getOrDefault(currentPercent.intValue(), new ArrayList<>());
+			wealthByPercent.putIfAbsent(currentPercent.intValue(), orDefault);
+			orDefault.add(list.get(i));
+		});
+		return wealthByPercent;
 	}
 
 }
